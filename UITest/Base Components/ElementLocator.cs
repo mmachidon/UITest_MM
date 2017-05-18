124ee5ffc89b87927e197fde01b0a9f721972614
mmachidon <m.machidon1@gmail.com>
2017-05-18 11:40:42 +0300

    Added ElementLocator cs

---
+++ UITest/Base Components/ElementLocator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UITest.Base_Components
+{
+    public enum Locator
+    {
+        Id,
+        CssSelector,
+        LinkText,
+        Name,
+        TagName,
+        XPath
+    }
+    public class ElementLocator
+    {
+        public ElementLocator(Locator type, string value)
+        {
+            this.Type= type;
+            this.Value = value;
+        }
+        public Locator Type { get; set; }
+        public string Value { get; set; }  
+      
+    }
+
+}
